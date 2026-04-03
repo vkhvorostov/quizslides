663157182e7dc8a7cbdec30ccc1a2ee7e400e6cb
HIGHB4CK <155472874+HIGHB4CK@users.noreply.github.com>
2026-04-03 13:53:40 +0700

    Index page (#35)
    * feat: add endpoint for index page
    
    * Made a sketch of index_page
    
    * feat: import bootstrap5
    
    fix: extend base.html in index.html and implement bootstrap styles
    
    * refactor: extract header to includes and clean up base.html
    
    ---------
    
    Co-authored-by: Veteroch4k <indefinite05@mail.ru>
    Co-authored-by: back high <highback123@gmail.com>
    Co-authored-by: Vladimir Khvorostov <vladimir@khvorostov.ru>

--- quizslides/urls.py
+++ quizslides/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
+from quizslides.views import Index
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='accounts:login', permanent=False)),
+    path('', Index.as_view(), name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
