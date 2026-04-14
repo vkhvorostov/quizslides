281228635f356c2738c89554007fcf8c50dbf2c7
Вадим Зенцов <vadimzentsov65@gmail.com>
2026-04-14 13:20:50 +0700

    fix button "Exit"

--- quizslides/settings.py
+++ quizslides/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'quizslides_bd',        
         'USER': 'postgres',
-        'PASSWORD': 'GroupconnectedDB',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
