5f579b52a7ccfe0eb9aec0f71b48fb6e3013b7f7
eviltyan <vlabuzova@gmail.com>
2026-05-12 09:58:53 +0700

    Настройки переведены в исходное состояние

--- quizslides/settings.py
+++ quizslides/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'quizslides_bd'),
         'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'Gfkflby1'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'GroupconnectedDB'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
